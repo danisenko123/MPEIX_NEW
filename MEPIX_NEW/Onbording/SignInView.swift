3f40af2b8520768f08f58d714a4108715b8ea723
d <anisenko.95@mail.ru>
2022-03-31 17:42:29 +0300

    Добавил текст

--- MEPIX_NEW/Onbording/SignInView.swift
+++ MEPIX_NEW/Onbording/SignInView.swift
@@ -11,6 +11,12 @@ struct SignInView: View {
     @State private var goups: String = ""
     var body: some View {
         VStack{
+            Text("Поиск расписания")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 30)
+                Text("Введите номер группы или имя преподователя и мы загрузим выбранное рассписание на устройство")
+            
             TextField("Видите группу", text: $goups)
                 .font(.title3)
                 .padding()
@@ -19,6 +25,7 @@ struct SignInView: View {
                 .cornerRadius(50.0)
                 .shadow(color: Color.black.opacity(0.08), radius: 60, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 16)
                 .padding(.vertical)
+            
             
             
         }
